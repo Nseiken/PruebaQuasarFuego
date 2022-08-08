339eae672f645c4cbee54bb0bb9c7efee99676c3
Cristian Garcia <ncgarcias@hotmail.com>
2022-08-07 19:15:23 -0500

    Prepare webserver

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// const QuasarController = require('./controllers/QuasarController');
-// Establecer el MVC con las rutas
-// Establecer los servicios con los métodos getLocation y getMessage
-// Separar distancias de los vectores del satélite
-const main = async() => {
-    // let satellites = new QuasarController().createSatellites();
-    // let positionMessage = new QuasarController().findPositionSatellites();
-    // console.log(positionMessage);
-    // let finalMessage =  new QuasarController().getMessagesFromFile();
-    // console.log(finalMessage);
-}
+const express = require('express')
+const app = express()
+require('dotenv').config();
+const port = process.env.PORT;
 
-main();
+app.get('/', function (req, res) {
+  res.send('Hello World NODEJS EXPRESS')
+})
+
+app.listen(port)
+
+console.log('Escuchando el puerto', port);
