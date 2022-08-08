0381d8328300a5a89a0ffe44af8143f1114109b4
Cristian Garcia <ncgarcias@hotmail.com>
2022-08-08 06:15:26 -0500

    port set manually

--- models/Server.js
+++ models/Server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || 8080;
+        this.port = 8080;
         this.path = '/api/quasar';
 
         // Config body request parser
